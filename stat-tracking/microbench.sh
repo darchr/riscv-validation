98134242be26eaa4935021198a6270aa2f38d4fd
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-03 21:39:47 +0000

    Calculate and log ipc in the CSV file

--- stat-tracking/microbench.sh
+++ stat-tracking/microbench.sh
@@ -4,15 +4,21 @@
 
 #!/bin/sh
 
-echo "Benchmark,instructions,cycles" > microbench.csv
+echo "Benchmark,instructions,cycles,ipc" > microbench.csv
 
 for dir in */ ; do
     PERF_DATA=$(perf stat -e cycles:u,instructions:u -o /dev/stdout $dir/bench)
+
     echo -n $dir | tr -d '/' >> microbench.csv
     echo -n "," >> microbench.csv
+
     INSTRUCTIONS=$(echo $PERF_DATA | grep -o -E '[0-9]+ instructions' | grep -o -E '[0-9]+' | tr -d '\n')
     echo -n $INSTRUCTIONS >> microbench.csv
     echo -n "," >> microbench.csv
+
     CYCLES=$(echo $PERF_DATA | grep -o -E '[0-9]+ cycles' | grep -o -E '[0-9]+')
-    echo $CYCLES >> microbench.csv
+    echo -n $CYCLES >> microbench.csv
+    echo -n "," >> microbench.csv
+
+    python3 -c "print($INSTRUCTIONS/$CYCLES)" >> microbench.csv
 done
