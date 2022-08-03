26a396354f71d2ec460ea06dfd4b08efd0a0c989
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-03 21:34:41 +0000

    Use variables to store cycle and instruction counts

--- stat-tracking/microbench.sh
+++ stat-tracking/microbench.sh
@@ -10,7 +10,9 @@ for dir in */ ; do
     PERF_DATA=$(perf stat -e cycles:u,instructions:u -o /dev/stdout $dir/bench)
     echo -n $dir | tr -d '/' >> microbench.csv
     echo -n "," >> microbench.csv
-    echo $PERF_DATA | grep -o -E '[0-9]+ instructions' | grep -o -E '[0-9]+' | tr -d '\n' >> microbench.csv
+    INSTRUCTIONS=$(echo $PERF_DATA | grep -o -E '[0-9]+ instructions' | grep -o -E '[0-9]+' | tr -d '\n')
+    echo -n $INSTRUCTIONS >> microbench.csv
     echo -n "," >> microbench.csv
-    echo $PERF_DATA | grep -o -E '[0-9]+ cycles' | grep -o -E '[0-9]+' >> microbench.csv
+    CYCLES=$(echo $PERF_DATA | grep -o -E '[0-9]+ cycles' | grep -o -E '[0-9]+')
+    echo $CYCLES >> microbench.csv
 done
