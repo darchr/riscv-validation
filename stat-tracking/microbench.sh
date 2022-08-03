6984d11efa1336bfdbbf7263e464a45489677bca
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-03 21:16:34 +0000

    Use comma instead of '/' after benchmark name in CSV

--- stat-tracking/microbench.sh
+++ stat-tracking/microbench.sh
@@ -8,7 +8,8 @@ echo "Benchmark,instructions,cycles" > microbench.csv
 
 for dir in */ ; do
     PERF_DATA=$(perf stat -e cycles:u,instructions:u -o /dev/stdout $dir/bench)
-    echo -n $dir >> microbench.csv
+    echo -n $dir | tr -d '/' >> microbench.csv
+    echo -n "," >> microbench.csv
     echo $PERF_DATA | grep -o -E '[0-9]+ instructions' | grep -o -E '[0-9]+' | tr -d '\n' >> microbench.csv
     echo -n "," >> microbench.csv
     echo $PERF_DATA | grep -o -E '[0-9]+ cycles' | grep -o -E '[0-9]+' >> microbench.csv
