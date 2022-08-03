bf19331d57d68fdfd678984846768d4c450eb416
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-03 13:16:51 -0700

    Update board to use CustomCPUTypes enum and channeled memory

--- newboard/hifivenew/HiFiveBoard.py
+++ newboard/hifivenew/HiFiveBoard.py
@@ -1,15 +1,14 @@
 from HiFiveCache import HiFiveCacheHierarchy
-from gem5.components.memory.single_channel import SingleChannelDDR4_2400
+from gem5.components.memory.dram_interfaces.ddr4 import DDR4_2400_8x8
+from gem5.components.memory.memory import ChanneledMemory
 from gem5.components.processors.simple_processor import SimpleProcessor
 from gem5.utils.requires import requires
 from gem5.isas import ISA
 from gem5.components.boards.simple_board import SimpleBoard
 from gem5.resources.resource import Resource
 from gem5.simulate.simulator import Simulator
-from gem5.components.processors.cpu_types import CPUTypes
-from gem5.components.memory.memory import ChanneledMemory
+from gem5.components.processors.cpu_types import CPUTypes, CustomCPUTypes
 
-#from gem5.components.memory.dram_interfaces.ddr4 import DDR4_2400_8x8
 #from m5.objects import AddrRange
 
 requires(isa_required=ISA.RISCV)
@@ -17,11 +16,10 @@ requires(isa_required=ISA.RISCV)
 cache_hierarchy = HiFiveCacheHierarchy(
     l1d_size="32kB", l1i_size="32kB", l2_size="2MB"
 )
-memory = SingleChannelDDR4_2400("16GB")
-#memory = ChanneledMemory(DDR4_2400_8x8, 1, 64)
+memory = ChanneledMemory(DDR4_2400_8x8, 1, 64)
 #memory.set_memory_range([AddrRange(start=0x80000000, size=16GB)])
 
-processor = SimpleProcessor(cpu_type=CPUTypes.U74, num_cores=1)
+processor = SimpleProcessor(cpu_type=CustomCPUTypes.U74, num_cores=1)
 #processor = U74Processor(cpu_type=CPUTypes.MINOR, num_cores=1)
 
 board = SimpleBoard(
