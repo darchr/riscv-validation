3cdf31e23e021ca000097758fc5ade1736e0a035
kunpai <kunpai@ucdavis.edu>
2022-08-08 14:16:24 -0700

    stats tracking % difference works; statsdump.csv is sample IPC tracking

--- stat-tracking/newStatsTrack.py
+++ stat-tracking/newStatsTrack.py
@@ -1,48 +1,49 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-# making dataframe 
-df1 = pd.read_csv("perf_microbench.csv")  
+# making dataframe
+df1 = pd.read_csv("perf_microbench.csv")
 X = df1["Benchmark"]
 Y1 = df1["IPC"]
 Y2 = df1["Cycles"]
 
-df2 = pd.read_csv("gem5runs_microbench.csv")  
+df2 = pd.read_csv("gem5runs_microbench.csv")
 X_2 = df2["Benchmark"]
 Y1_2 = df2["IPC"]
 Y2_2 = df2["Cycles"]
 
-with open('statsdump.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['perf', 'gem5', '% difference'])
+with open("statsdump.csv", "w") as csvfile:
+    filewriter = csv.writer(
+        csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+    )
+    filewriter.writerow(["perf", "gem5", "% difference"])
     csvfile.close()
-
 
 
 def plot(stat):
     i = 0
     j = 0
-    
+
     for b, bench in enumerate(X):
-        plt.bar(i-0.1, float(df1[stat].iloc[b]), 0.2, color='C'+str(j))
+        plt.bar(i - 0.1, float(df1[stat].iloc[b]), 0.2, color="C" + str(j))
         j = int(not j)
-        plt.bar(i+0.1, float(df2[stat].iloc[b]), 0.2, color='C'+str(j))
+        plt.bar(i + 0.1, float(df2[stat].iloc[b]), 0.2, color="C" + str(j))
         j = int(not j)
         i = i + 1
-        diff = (100*(df2[stat].iloc[b] - df1[stat].iloc[b]))/(df2[stat].iloc[b])
-        with open('statsdump.csv', 'a') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        diff = (100 * (df2[stat].iloc[b] - df1[stat].iloc[b])) / (df2[stat].iloc[b])
+        with open("statsdump.csv", "a") as csvfile:
+            filewriter = csv.writer(
+                csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+            )
             filewriter.writerow([df1[stat].iloc[b], df2[stat].iloc[b], diff])
             csvfile.close()
-    
-    for i,pfrm in enumerate(['perf', 'gem5']):
-        plt.bar(0,0,color='C'+str(i), label=pfrm)
 
-    plt.xticks((np.arange(len(X))), X, rotation=80, ha='center',fontsize=11)
+    for i, pfrm in enumerate(["perf", "gem5"]):
+        plt.bar(0, 0, color="C" + str(i), label=pfrm)
+
+    plt.xticks((np.arange(len(X))), X, rotation=80, ha="center", fontsize=11)
 
     plt.xlabel("Benchmarks")
     plt.ylabel(stat)
@@ -50,7 +51,5 @@ def plot(stat):
     plt.legend()
     plt.show()
 
+
 plot("IPC")
-
-
-
