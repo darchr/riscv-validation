0bc14af1f4c80c92940bb668840313abcfeb6dc7
kunpai <kunpai@ucdavis.edu>
2022-08-08 14:12:06 -0700

    stats tracking % difference works; statsdump.csv is sample IPC tracking

--- stat-tracking/newStatsTrack.py
+++ stat-tracking/newStatsTrack.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
- # making dataframe 
+import csv
+
+# making dataframe 
 df1 = pd.read_csv("perf_microbench.csv")  
 X = df1["Benchmark"]
 Y1 = df1["IPC"]
@@ -11,6 +13,14 @@ df2 = pd.read_csv("gem5runs_microbench.csv")
 X_2 = df2["Benchmark"]
 Y1_2 = df2["IPC"]
 Y2_2 = df2["Cycles"]
+
+with open('statsdump.csv', 'w') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['perf', 'gem5', '% difference'])
+    csvfile.close()
+
+
 
 def plot(stat):
     i = 0
@@ -22,6 +32,12 @@ def plot(stat):
         plt.bar(i+0.1, float(df2[stat].iloc[b]), 0.2, color='C'+str(j))
         j = int(not j)
         i = i + 1
+        diff = (100*(df2[stat].iloc[b] - df1[stat].iloc[b]))/(df2[stat].iloc[b])
+        with open('statsdump.csv', 'a') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            filewriter.writerow([df1[stat].iloc[b], df2[stat].iloc[b], diff])
+            csvfile.close()
     
     for i,pfrm in enumerate(['perf', 'gem5']):
         plt.bar(0,0,color='C'+str(i), label=pfrm)
@@ -34,6 +50,7 @@ def plot(stat):
     plt.legend()
     plt.show()
 
-plot("Cycles")
+plot("IPC")
+
 
 
