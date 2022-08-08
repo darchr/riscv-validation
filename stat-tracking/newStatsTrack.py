dcf13a703c9d4713f84bda2f6b05728736495644
kunpai <kunpai@ucdavis.edu>
2022-08-08 10:28:57 -0700

    Changes to improve display

--- stat-tracking/newStatsTrack.py
+++ stat-tracking/newStatsTrack.py
@@ -1,32 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
-
+import numpy as np
  # making dataframe 
 df1 = pd.read_csv("perf_microbench.csv")  
 X = df1["Benchmark"]
 Y1 = df1["IPC"]
 Y2 = df1["Cycles"]
-  
-# # X_axis = np.arange(len(X))
-# plt.bar(df1["Benchmark"], df1["IPC"], color= 'blue')
-# plt.bar(1, Y1, 0.4, label = X)
-  
-# plt.xticks(X_axis, X)
-# plt.xlabel("Groups")
-# plt.ylabel("Number of Students")
-# plt.title("Number of Students in each group")
-# plt.legend()
-# plt.show()
 
-for i in range(len(df1)):
-    #print(df1["Benchmark"][i])
-    plt.bar(i+0.1, df1["IPC"], 0.2, color= 'blue', label = "Perf " + df1["Benchmark"][i])
-    plt.bar(i-0.1, df1["IPC"], 0.2,  color= 'red', label = "gem5 " + df1["Benchmark"][i])
-    break
+df2 = pd.read_csv("gem5runs_microbench.csv")  
+X_2 = df2["Benchmark"]
+Y1_2 = df2["IPC"]
+Y2_2 = df2["Cycles"]
+
+def plot(stat):
+    i = 0
+    j = 0
+    
+    for b, bench in enumerate(X):
+        plt.bar(i-0.1, float(df1[stat].iloc[b]), 0.2, color='C'+str(j))
+        j = int(not j)
+        plt.bar(i+0.1, float(df2[stat].iloc[b]), 0.2, color='C'+str(j))
+        j = int(not j)
+        i = i + 1
+    
+    for i,pfrm in enumerate(['perf', 'gem5']):
+        plt.bar(0,0,color='C'+str(i), label=pfrm)
+
+    plt.xticks((np.arange(len(X))), X, rotation=80, ha='center',fontsize=11)
+
+    plt.xlabel("Benchmarks")
+    plt.ylabel(stat)
+    plt.title("Benchmark vs. " + stat)
+    plt.legend()
+    plt.show()
+
+plot("Cycles")
 
 
-plt.xlabel("Benchmarks")
-plt.ylabel("IPC")
-plt.title("Benchmark vs. IPC")
-plt.legend()
-plt.show()
