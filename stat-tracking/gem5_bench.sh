793a42b5658092cec35dff343aeb594c251e7439
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-11 09:45:36 -0700

    Update gem5 benchmark script to take RISCV binaries directory as CLI input 2

--- stat-tracking/gem5_bench.sh
+++ stat-tracking/gem5_bench.sh
@@ -3,26 +3,27 @@
 
 # REQUIREMENTS:
 # This script must be run from the root gem5 source directory
-# The benchmark binaries must be located in ./bins/ and have extension .RISCV
-# USAGE: ./gem5_microbench.sh <path_to_gem5_config_script> <argv[1]>
+# The benchmark binaries must be located in directory $2
+# USAGE: ./gem5_microbench.sh <path_to_gem5_config_script> <path_to_RISCV_binaries_dir> <argv[1]>
 # example:
-    # ./gem5_microbench.sh ./src/python/gem5/prebuilt/hifivenew/HiFiveRun.py 10
+    # ./gem5_microbench.sh ./src/python/gem5/prebuilt/hifivenew/HiFiveRun.py microbenchmarks 10
 
 #!/bin/sh
 
-echo "Benchmark,instructions,cycles,ipc" > gem5_microbench.csv
+CSV=$(echo $2 | tr -d ./)
+echo "Benchmark,instructions,cycles,ipc" > gem5_$CSV.csv
 
-for bin in bins/* ; do
-    ./build/RISCV/gem5.opt $1 --riscv_binary=$bin --argv=$2
+for bin in $2/* ; do
+    ./build/RISCV/gem5.opt $1 --riscv_binary=$bin --argv=$3
 
-    echo -n $(basename $bin .RISCV), >> gem5_microbench.csv
+    echo -n $(basename $bin .RISCV), >> gem5_$CSV.csv
 
     INSTRUCTIONS=$(grep board.processor.cores.core.numInsts m5out/stats.txt | grep -o -E '[0-9]+')
-    echo -n $INSTRUCTIONS, >> gem5_microbench.csv
+    echo -n $INSTRUCTIONS, >> gem5_$CSV.csv
 
     CYCLES=$(grep numCycles m5out/stats.txt | grep -o -E '[0-9]+')
-    echo -n $CYCLES, >> gem5_microbench.csv
+    echo -n $CYCLES, >> gem5_$CSV.csv
 
     IPC=$(grep ipc m5out/stats.txt | grep -o -E '0.[0-9]+')
-    echo $IPC >> gem5_microbench.csv
+    echo $IPC >> gem5_$CSV.csv
 done
