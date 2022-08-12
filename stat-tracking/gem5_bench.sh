dc81302b1930aada3c5de40115801afbd3f9feec
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-12 12:05:36 -0700

    Simplify gem5_bench script code for possible parallelization

--- stat-tracking/gem5_bench.sh
+++ stat-tracking/gem5_bench.sh
@@ -4,26 +4,27 @@
 # REQUIREMENTS:
 # This script must be run from the root gem5 source directory
 # The benchmark binaries must be located in directory $2
-# USAGE: ./gem5_microbench.sh <path_to_gem5_config_script> <path_to_RISCV_binaries_dir> <argv[1]>
+# USAGE: ./gem5_bench.sh <path_to_gem5_config_script> <path_to_RISCV_binaries_dir> <argv[1]>
 # example:
     # ./gem5_microbench.sh ./src/python/gem5/prebuilt/hifivenew/HiFiveRun.py microbenchmarks 10
 
 #!/bin/sh
+run_sim () {
+    BENCH=$(basename $1 .RISCV)
 
-CSV=$(echo $2 | tr -d ./)
-echo "Benchmark,instructions,cycles,ipc" > gem5_$CSV.csv
-
-for bin in $2/* ; do
-    ./build/RISCV/gem5.opt $1 --riscv_binary=$bin --argv=$3
-
-    echo -n $(basename $bin .RISCV), >> gem5_$CSV.csv
+    ./build/RISCV/gem5.opt $1 --riscv_binary=$2 --argv=$3
 
     INSTRUCTIONS=$(grep board.processor.cores.core.numInsts m5out/stats.txt | grep -o -E '[0-9]+')
-    echo -n $INSTRUCTIONS, >> gem5_$CSV.csv
 
     CYCLES=$(grep numCycles m5out/stats.txt | grep -o -E '[0-9]+')
-    echo -n $CYCLES, >> gem5_$CSV.csv
 
     IPC=$(grep ipc m5out/stats.txt | grep -o -E '0.[0-9]+')
-    echo $IPC >> gem5_$CSV.csv
+    echo $BENCH,$INSTRUCTIONS,$CYCLES,$IPC >> gem5_$SUITE.csv
+}
+
+SUITE=$(echo $2 | tr -d ./)
+echo "Benchmark,instructions,cycles,ipc" > gem5_$SUITE.csv
+
+for bin in $2/* ; do
+    run_sim $1 $bin $3
 done
