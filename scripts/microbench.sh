90742ccb66a944d58cbcef306a6fb3070ba5495a
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-16 17:26:00 +0000

    Refactor to have less code

--- scripts/microbench.sh
+++ scripts/microbench.sh
@@ -10,21 +10,13 @@ for dir in */ ; do
     echo "Running $dir"
     PERF_DATA=$(perf stat -r 1 -e cycles:u,instructions:u -o /dev/stdout $dir/bench.RISCV)
 
-    echo -n $dir | tr -d '/' >> microbench.csv
-    echo -n "," >> microbench.csv
-
     INSTRUCTIONS=$(echo $PERF_DATA | grep -o -E '[0-9]+ instructions' | grep -o -E '[0-9]+' | tr -d '\n')
-    echo -n $INSTRUCTIONS >> microbench.csv
-    echo -n "," >> microbench.csv
 
     CYCLES=$(echo $PERF_DATA | grep -o -E '[0-9]+ cycles' | grep -o -E '[0-9]+')
-    echo -n $CYCLES >> microbench.csv
-    echo -n "," >> microbench.csv
 
     SECONDS=$(echo $PERF_DATA | grep -o -E "[0-9]+.[0-9]+ seconds time elapsed" | grep -o -E '[0-9]+.[0-9]+')
-    echo -n $SECONDS >> microbench.csv
-    echo -n "," >> microbench.csv
 
+    echo -n $dir,$INSTRUCTIONS,$CYCLES,$SECONDS, | tr -d '/' >> microbench.csv
     python3 -c "print($INSTRUCTIONS/$CYCLES, end=',')" >> microbench.csv
     python3 -c "print($INSTRUCTIONS/$SECONDS)" >> microbench.csv
 done
