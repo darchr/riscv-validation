cdebdaeb3e03f4c527f854ede07c903c273c472e
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-31 22:08:09 +0000

    Add script to run all spec 2006 benchmarks that are also available in gem5 resources (excluding gamess and wrf due to build failures on HiFive Unmatched

---
+++ scripts/perf_spec.sh
@@ -0,0 +1,23 @@
+# USAGE: ./perf_spec.sh <perf_binary> <spec_input_size>
+# depends on gnu-parallel: sudo apt install parallel
+# spec must be compiled and initialized with `source shrc`
+# Runs benchmarks listed on http://resources.gem5.org/resources/spec-2006 except
+# for 416.gamess and 481.wrf due to spec build errors with those benchmarks
+# the number of threads is limited to 4, U740 processor has 4 accessible cores
+
+#!/bin/bash
+
+OUTDIR=perf-spec-out
+mkdir -p $OUTDIR
+echo "Benchmark,Instructions,Cycles,Seconds,Branches,Branch-misses" > $OUTDIR/perf_spec2006.csv
+
+parallel -j4 $1 stat -e instructions,cycles,branches,branch-misses -o $OUTDIR/{}.out runspec --size $2 --iterations 1 --noreportable --config riscv.cfg --nobuild {} ::: {bzip2,gcc,bwaves,mcf,milc,zeusmp,gromacs,cactusADM,leslie3d,namd,gobmk,povray,calculix,hmmer,sjeng,GemsFDTD,libquantum,h264ref,tonto,lbm,omnetpp,astar,sphinx3,998.specrand,999.specrand}
+
+for bench in $OUTDIR/*.out ; do
+    INSTRUCTIONS=$(grep -o -E '[0-9]+\s+instructions' $bench | grep -o -E '[0-9]+')
+    CYCLES=$(grep -o -E '[0-9]+\s+cycles' $bench | grep -o -E '[0-9]+')
+    TIMESEC=$(grep -o -E '[0-9]+.[0-9]+\s?\+?\-?\s?[0-9]+?.?[0-9]+?\s+seconds time elapsed' $bench | grep -o -E '[0-9]+.[0-9]+' | head -n1)
+    BRANCHES=$(grep -o -E '[0-9]+\s+branches' $bench | grep -o -E '[0-9]+')
+    BRANCH_MISSES=$(grep -o -E '[0-9]+\s+branch-misses' $bench | grep -o -E '[0-9]+')
+    echo $(basename --suffix=.out $bench),$INSTRUCTIONS,$CYCLES,$TIMESEC,$BRANCHES,$BRANCH_MISSES >> $OUTDIR/perf_spec2006.csv
+done
