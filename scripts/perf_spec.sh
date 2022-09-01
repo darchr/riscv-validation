1a465cf5e9466fb8249abf84b749bb8da1762fd7
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-09-01 21:24:53 +0000

    Use bash loop instead of gnu-parallel for serial exection of SPEC benchmarks

--- scripts/perf_spec.sh
+++ scripts/perf_spec.sh
@@ -8,12 +8,18 @@
 
 #!/bin/bash
 
-OUTDIR=perf-spec-out
+OUTDIR=perf-spec-$2-out
 mkdir -p $OUTDIR
 echo "Benchmark,Instructions,Cycles,Seconds,Branches,Branch-misses" > $OUTDIR/perf_spec2006.csv
 
-parallel -j1 $1 stat -e instructions,cycles,branches,branch-misses -o $OUTDIR/{}.out runspec --size $2 --iterations 1 --noreportable --config riscv.cfg --nobuild {} ::: {bzip2,gcc,bwaves,mcf,milc,zeusmp,gromacs,cactusADM,leslie3d,namd,gobmk,povray,calculix,hmmer,sjeng,GemsFDTD,libquantum,h264ref,tonto,lbm,omnetpp,astar,sphinx3,998.specrand,999.specrand}
+BENCHES="bzip2 gcc bwaves mcf milc zeusmp gromacs cactusADM leslie3d namd gobmk povray calculix hmmer sjeng GemsFDTD libquantum h264ref tonto lbm omnetpp astar sphinx3 998.specrand 999.specrand"
 
+# Run all SPEC 2006 benchmarks in BENCHES
+for bench in $BENCHES; do
+    $1 stat -e instructions,cycles,branches,branch-misses -o $OUTDIR/$bench.out runspec --size $2 --iterations 1 --noreportable --config riscv.cfg --nobuild $bench
+done
+
+# collect statistics
 for bench in $OUTDIR/*.out ; do
     INSTRUCTIONS=$(grep -o -E '[0-9]+\s+instructions' $bench | grep -o -E '[0-9]+')
     CYCLES=$(grep -o -E '[0-9]+\s+cycles' $bench | grep -o -E '[0-9]+')
